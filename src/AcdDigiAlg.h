c1be8d757e951952900990a2f9218bc9b78a6c2e
heather <>
2002-06-05 13:22:01 +0000

    store energy as well - as a check on pha

--- src/AcdDigiAlg.h
+++ src/AcdDigiAlg.h
@@ -11,7 +11,7 @@ namespace xml { class IFile; }
   * This algorithm does the work of creating the AcdDigi objects and storing 
   * them on the TDS.
   *
-  * $Header$
+  * $Header: /nfs/slac/g/glast/ground/cvs/AcdDigi/src/AcdDigiAlg.h,v 1.1.1.1 2002/05/23 17:53:37 heather Exp $
   */
 
 class AcdDigiAlg : public Algorithm {
@@ -25,11 +25,11 @@ public:
 
 private:
 
-    /// activates PHA
+    /// Low discrim threshold which activates PHA
     double m_lowThreshold;  
-    /// CNO
+    /// High discrim threshold for CNO
     double m_highThreshold;
-    /// nominal veto signal
+    /// Veto discrim threshold for nominal veto signal
     double m_vetoThreshold; 
     /// conversion from energy to ADC channels
     double m_adcChannelsPerMeV;
