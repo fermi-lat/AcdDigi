188af0b3e2fb0c6aaa14c51517be275fd400fb23
heather <>
2002-07-26 17:52:39 +0000

    add new alg to list

--- src/Dll/AcdDigi_load.cxx
+++ src/Dll/AcdDigi_load.cxx
@@ -3,12 +3,13 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header$
+*  $Header: /nfs/slac/g/glast/ground/cvs/AcdDigi/src/Dll/AcdDigi_load.cxx,v 1.1.1.1 2002/05/23 17:53:37 heather Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(AcdDigi) {
     DECLARE_ALGORITHM( AcdDigiAlg );
+    DECLARE_ALGORITHM( AcdDigiOrgAlg );
 } 
 
