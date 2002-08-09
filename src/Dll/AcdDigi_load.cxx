db92f910df9c666702789e870b57bfa58653a613
heather <>
2002-08-09 19:13:22 +0000

    leave algorithm using McIntegratingHit as an option

--- src/Dll/AcdDigi_load.cxx
+++ src/Dll/AcdDigi_load.cxx
@@ -3,13 +3,14 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/AcdDigi/src/Dll/AcdDigi_load.cxx,v 1.1.1.1 2002/05/23 17:53:37 heather Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/AcdDigi/src/Dll/AcdDigi_load.cxx,v 1.2 2002/07/26 17:52:39 heather Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(AcdDigi) {
     DECLARE_ALGORITHM( AcdDigiAlg );
+    DECLARE_ALGORITHM( AcdDigiMcIntHitAlg );
     DECLARE_ALGORITHM( AcdDigiOrgAlg );
 } 
 
