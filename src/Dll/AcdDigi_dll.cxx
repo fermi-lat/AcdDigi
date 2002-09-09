1af43ca4241d5a354cdee864a20bf849b1c5354a
heather <>
2002-09-09 16:45:55 +0000

    update doc

--- src/Dll/AcdDigi_dll.cxx
+++ src/Dll/AcdDigi_dll.cxx
@@ -1,6 +1,6 @@
-/** @file AcdDigi_dll.cpp
+/** @file AcdDigi_dll.cxx
 
-  $Header$
+  $Header: /nfs/slac/g/glast/ground/cvs/AcdDigi/src/Dll/AcdDigi_dll.cxx,v 1.1.1.1 2002/05/23 17:53:37 heather Exp $
 */
 
 #include "GaudiKernel/LoadFactoryEntries.h"
--- src/Dll/AcdDigi_load.cxx
+++ src/Dll/AcdDigi_load.cxx
@@ -1,9 +1,9 @@
 /** 
-* @file AcdDigi_load.cpp
+* @file AcdDigi_load.cxx
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/AcdDigi/src/Dll/AcdDigi_load.cxx,v 1.2 2002/07/26 17:52:39 heather Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/AcdDigi/src/Dll/AcdDigi_load.cxx,v 1.3 2002/08/09 19:13:22 heather Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
