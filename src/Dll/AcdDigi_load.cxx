a4d95807e21644bc379dba5ec34f4ccd756166f1
kyoung <>
2002-10-08 22:11:20 +0000

    Add AcdDigiRandom tool

--- src/Dll/AcdDigi_load.cxx
+++ src/Dll/AcdDigi_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/AcdDigi/src/Dll/AcdDigi_load.cxx,v 1.3 2002/08/09 19:13:22 heather Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/AcdDigi/src/Dll/AcdDigi_load.cxx,v 1.4 2002/09/09 16:43:05 heather Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -12,5 +12,6 @@ DECLARE_FACTORY_ENTRIES(AcdDigi) {
     DECLARE_ALGORITHM( AcdDigiAlg );
     DECLARE_ALGORITHM( AcdDigiMcIntHitAlg );
     DECLARE_ALGORITHM( AcdDigiOrgAlg );
+    DECLARE_TOOL( AcdDigiRandom );
 } 
 
