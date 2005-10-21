3e534394dca5c09b9a1ffedefdbe7c74e7d11469
heather <>
2005-10-21 22:49:30 +0000

    patch to completely remove AcdDigiMcIntAlg

--- src/Dll/AcdDigi_load.cxx
+++ src/Dll/AcdDigi_load.cxx
@@ -3,14 +3,13 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/AcdDigi/src/Dll/AcdDigi_load.cxx,v 1.4 2002/09/09 16:43:05 heather Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/AcdDigi/src/Dll/AcdDigi_load.cxx,v 1.5 2002/10/08 22:11:20 kyoung Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(AcdDigi) {
     DECLARE_ALGORITHM( AcdDigiAlg );
-    DECLARE_ALGORITHM( AcdDigiMcIntHitAlg );
     DECLARE_ALGORITHM( AcdDigiOrgAlg );
     DECLARE_TOOL( AcdDigiRandom );
 } 
