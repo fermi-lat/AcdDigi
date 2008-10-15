2d31cde589dee1ea62d34f41c25f67a7df87b8b5
usher <>
2008-10-15 15:45:42 +0000

    Initial import AcdDigiMergeAlg for merging AcdDigis from background events with those from simulation

--- src/Dll/AcdDigi_load.cxx
+++ src/Dll/AcdDigi_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/AcdDigi/src/Dll/AcdDigi_load.cxx,v 1.5 2002/10/08 22:11:20 kyoung Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/AcdDigi/src/Dll/AcdDigi_load.cxx,v 1.6 2005/10/21 22:49:30 heather Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -11,6 +11,7 @@
 DECLARE_FACTORY_ENTRIES(AcdDigi) {
     DECLARE_ALGORITHM( AcdDigiAlg );
     DECLARE_ALGORITHM( AcdDigiOrgAlg );
+    DECLARE_ALGORITHM( AcdDigiMergeAlg );
     DECLARE_TOOL( AcdDigiRandom );
 } 
 
